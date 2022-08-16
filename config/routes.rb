097d86ed4cb07035e2ae9ed3a4a37f5009267ee3
Fabian Schmidt <fabian@my-schmidt.de>
2022-08-16 13:10:27 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get "tasks/new", to: "tasks#new"
   get "tasks/:id", to: "tasks#show", as: :task
   post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit"
 end
